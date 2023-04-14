a94df4b27f8e338256682e93584b05c64fdc742a
Eric Brohman <ericbrohman@gmail.com>
2023-04-14 15:42:34 -0600

    Create user accounts

---
+++ db/migrate/20230414213229_user_accounts.rb
@@ -0,0 +1,8 @@
+class UserAccounts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accounts do |t|
+      t.decimal :balance, scale: 2, precision: 8
+      t.references :user, foreign_key: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_210407) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_213229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.decimal "balance", precision: 8, scale: 2
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -24,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_210407) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "accounts", "users"
 end
