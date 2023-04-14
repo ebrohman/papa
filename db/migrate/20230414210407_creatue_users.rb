3d117a64342b0f2fe41636249a920f49d6513726
Eric Brohman <ericbrohman@gmail.com>
2023-04-14 15:17:41 -0600

    Create Users table

---
+++ db/migrate/20230414210407_creatue_users.rb
@@ -0,0 +1,11 @@
+class CreatueUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email, index: { unique: true }
+      t.string :first_name
+      t.string :last_name
+      t.string :password_digest, unique: true
+      t.string :auth_token, index: { unique: true }
+    end
+  end
+end
