1789d1df1adc6556b3b3afcd77a8ba661103ce69
ebrohman <ericbrohman@gmail.com>
2023-04-15 17:25:25 -0600

    Frozen string lint

--- db/migrate/20230415224059_alter_table_visits.rb
+++ db/migrate/20230415224059_alter_table_visits.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AlterTableVisits < ActiveRecord::Migration[7.0]
   def change
     add_column :visits, :rate, :decimal, null: false, precision: 8, scale: 2, default: 1
--- db/migrate/20230415224550_create_table_transactions.rb
+++ db/migrate/20230415224550_create_table_transactions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateTableTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
