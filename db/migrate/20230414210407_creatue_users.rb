c3adec3d2209bf7d2325eb7ef10fe8944e851af4
ebrohman <ericbrohman@gmail.com>
2023-04-15 16:32:17 -0600

    Lint some files

--- db/migrate/20230414210407_creatue_users.rb
+++ db/migrate/20230414210407_creatue_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreatueUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
--- db/migrate/20230414221830_roles.rb
+++ db/migrate/20230414221830_roles.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Roles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
