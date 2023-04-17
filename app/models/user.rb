760f47d6cec9368b6d90d38da338ebff2594af20
Eric Brohman <ericbrohman@gmail.com>
2023-04-17 08:41:23 -0600

    Remove code from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,4 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_one :account
-
-  # filter out password digest
-  def as_json(options = {})
-    super(options).except('password_digest')
-  end
 end
