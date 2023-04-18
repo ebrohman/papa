17886f7afdc169dbec5875fbcf72439592514c0a
Eric Brohman <ericbrohman@gmail.com>
2023-04-18 08:32:52 -0600

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+member1 = User.create!(email: 'jim@papa.com')
+member2 = User.create!(email: 'bill@papa.com')
+
+member1.create_account(balance: 100)
+member2.create_account(balance: 100.0)
+
+visit1 = Visit.create!(member: member1,
+                       minutes: 30,
+                       rate: 1,
+                       date: DateTime.current + 30.days)
+
+completed_visit = Visit.create!(member: member1,
+                                minutes: 30,
+                                rate: 1,
+                                date: DateTime.current - 1.day,
+                                completed_at: DateTime.current)
+
+Transaction.create!(member: member1,
+                    pal: member2,
+                    amount: 30)
